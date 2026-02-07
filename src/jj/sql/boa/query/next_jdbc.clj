6c8aaa856e186807a2f02cc1d963d1951c3d3aa2
jj <keistaslogairkosulys@gmail.com>
2026-02-07 13:01:45 +0100

    Move common logic to build-query fn in boa
    # Conflicts:
    #	src/jj/sql/boa/query/next_jdbc.clj

--- src/jj/sql/boa/query/next_jdbc.clj
+++ src/jj/sql/boa/query/next_jdbc.clj
@@ -1,13 +1,16 @@
 (ns jj.sql.boa.query.next-jdbc
-  (:require [next.jdbc :as jdbc]
             [next.jdbc.result-set :as rs]
             [jj.sql.boa.query :refer [BoaQuery]]))
 
 (defrecord NextJdbcAdapter [additional-info]
   BoaQuery
   (build-parameterless-query [this ds sql]
+    (when (logger/enabled? :debug)
+      (logger/debugf "Query is: %s" [sql]))
     (jdbc/execute! ds [sql] additional-info))
   (build-query [this ds sql params]
+    (when (logger/enabled? :debug)
+      (logger/debugf "Query is: %s" (into [sql] params)))
     (jdbc/execute! ds (into [sql] params) additional-info)))
 
 (defn ->NextJdbcAdapter
