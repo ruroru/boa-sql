b0f970145f6516a699b1c6df0cbd8f86d678fdec
ruroru <111705692+ruroru@users.noreply.github.com>
2026-02-23 21:59:49 +0000

    Stepping project version for the next development cycle

--- next-jdbc-async-adapter/project.clj
+++ next-jdbc-async-adapter/project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.jj/next-jdbc-async-adapter "1.0.5"
+(defproject org.clojars.jj/next-jdbc-async-adapter "1.0.6-SNAPSHOT"
   :description "Boa SQL adapter for Next-JDBC"
   :url "https://github.com/ruroru/boa-sql"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojars.jj/boa-async-query "1.0.5"]
+  :dependencies [[org.clojars.jj/boa-async-query "1.0.6-SNAPSHOT"]
                  [com.github.seancorfield/next.jdbc "1.3.1093"]
                  [org.clojure/tools.logging "1.3.1"]]
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/boa-sql-parent "1.0.5"
+(defproject org.clojars.jj/boa-sql-parent "1.0.6-SNAPSHOT"
   :description "A library for frictionless SQL"
   :url "https://github.com/ruroru/boa-sql"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
